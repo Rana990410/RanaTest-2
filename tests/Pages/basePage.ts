8af780eabe6038be6d4007266fd988e5e5c31e02
Rana990410 <rana.m.mahmoud.4@gmail.com>
2025-12-19 15:24:01 +0200

    New Terminal Playwright

--- tests/Pages/basePage.ts
+++ tests/Pages/basePage.ts
@@ -15,7 +15,7 @@ export default class BasePage {
     await element.click();
   }
    public async takePageScreenshot(path: string) {
-    await this.page.screenshot({ path });
+    await this.page.screenshot({ path });// test edit
   }
    public async takePageVideo(path: string) {
     const video = this.page.video();
